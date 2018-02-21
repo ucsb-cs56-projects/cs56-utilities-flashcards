e53d8966cb374df436797fb8f6510909d0396831
adamfarnsworth@umail.ucsb.edu <atfarnsworth@Adam.localdomain>
2018-02-20 16:29:35 -0800

    100 points FIXED Flip button is not resetted after next button is clicked

--- src/edu/ucsb/cs56/projects/utilities/flashcards/CreateDeckFrame.java
+++ src/edu/ucsb/cs56/projects/utilities/flashcards/CreateDeckFrame.java
@@ -218,17 +218,21 @@ public class CreateDeckFrame extends JFrame {
 				cardNum = 0;
 			
 
+/*
+ * leave this commented out code for now, might fix some next and flip combos later
+ */
 //			if (flipCardButton.isSelected()) {
-//				flippedFlag = true;
-//				outer.showCard();
-//				System.out.println("first");
-//			} else {
-//				outer.showCard();
 //				flippedFlag = false;
-//				System.out.println("second");
+//				flipCardButton.setSelected(false);			
+//				outer.showCard();
+//			} else {			
+//				flippedFlag = false;
+//				outer.showCard();
 //			}
-
-			flippedFlag = false;
+			
+			
+			flippedFlag = false;				//sets card to front when next is pressed			
+			flipCardButton.setSelected(false);	//resets button to not pressed			
 			outer.showCard();
 
 		}
